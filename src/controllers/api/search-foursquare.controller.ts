95c64622739ec11e55df8dac758cc956e5c7ed31
arthur <arthur.osipenko@gmail.com>
2017-11-14 22:36:03 +0200

    proxy foursquare response with params

--- src/controllers/api/search-foursquare.controller.ts
+++ src/controllers/api/search-foursquare.controller.ts
@@ -1,16 +1,23 @@
 import express = require('express');
 import https = require('https');
+import querystring = require('querystring');
 import { BaseController } from '../base.controller';
 
 class FoursquareController extends BaseController {
     public handler() {
         const options = {
             hostname: 'api.foursquare.com',
-            port: 443,
-            path: `/v2/venues/search?client_id=${process.env.FOURSQUARE_CLIENT_ID}&client_secret=${process.env.FOURSQUARE_CLIENT_SECRET}&v=20171101&ll=50.024628,36.365108&radius=500`,
+            path: '/v2/venues/search?' + querystring.stringify(Object.assign({},
+                this.req.query,
+                {
+                    'client_id': process.env.FOURSQUARE_CLIENT_ID,
+                    'client_secret': process.env.FOURSQUARE_CLIENT_SECRET
+                }),
+                '&', '=', {encodeURIComponent: (s: any) => s}),
             method: 'GET'
         };
 
+        console.log(options);
         const req = https.request(options, (res) => {
             this.res.writeHead(res.statusCode, res.headers);
             return res.pipe(this.res, {end: true});
